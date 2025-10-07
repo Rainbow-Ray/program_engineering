1ba5227912c4484a846fff658a6682763cfd7bac
Rainbow-Ray <vkkravets03@gmail.com>
2025-10-08 00:42:37 +0500

    All in one

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -6,13 +6,87 @@ import importlib.util
 import sys
 import numpy as np
 from PIL import Image
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-from main import preprocess_image
-from main import postprocess_image
-from main import model
-from main import device
-from main import io_file_input
 # streamlit run streamlit\streamlit.py
+from PIL import Image
+import io
+from skimage import io as sio
+import numpy as np
+import torch
+import torch.nn.functional as F
+from transformers import AutoModelForImageSegmentation
+from torchvision.transforms.functional import normalize
+
+
+model = AutoModelForImageSegmentation.from_pretrained("briaai/RMBG-1.4",trust_remote_code=True)
+
+def preprocess_image(im: np.ndarray, model_input_size: list) -> torch.Tensor:
+    if len(im.shape) < 3:
+        im = im[:, :, np.newaxis]
+    im_tensor = torch.tensor(im, dtype=torch.float32).permute(2,0,1)
+    im_tensor = F.interpolate(torch.unsqueeze(im_tensor,0), size=model_input_size, mode='bilinear')
+    image = torch.divide(im_tensor,255.0)
+    image = normalize(image,[0.5,0.5,0.5],[1.0,1.0,1.0])
+    return image
+
+def postprocess_image(result: torch.Tensor, im_size: list)-> np.ndarray:
+    result = torch.squeeze(F.interpolate(result, size=im_size, mode='bilinear') ,0)
+    ma = torch.max(result)
+    mi = torch.min(result)
+    result = (result-mi)/(ma-mi)
+    im_array = (result*255).permute(1,2,0).cpu().data.numpy().astype(np.uint8)
+    im_array = np.squeeze(im_array)
+    return im_array
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+def show_image(path:str):
+  img = Image.open(path)
+  img.show()
+
+#show_image("cat_walking.jpg")
+#show_image("girafe.jpg")
+#show_image("dog_fight.jpg")
+
+def prepare_input (path:str):
+  orig_im = sio.imread(path)
+  orig_im_size = orig_im.shape[0:2]
+  model_input_size = [1024, 1024]
+  image = preprocess_image(orig_im, model_input_size).to(device)
+  result=model(image)
+  result_image = postprocess_image(result[0][0], orig_im_size)
+  pil_mask_im = Image.fromarray(result_image)
+  orig_image = Image.open(path)
+  no_bg_image = orig_image.copy()
+  no_bg_image.putalpha(pil_mask_im)
+  no_bg_image.show()
+
+# def io_file_input(file_object):
+#     orig_image = file_object
+#     orig_im = np.array(orig_image)
+#     orig_im_size = orig_im.shape[0:2]
+#     model_input_size = [1024, 1024]
+#     image = preprocess_image(orig_im, model_input_size).to(device)
+#     result = model(image)
+#     result_image = postprocess_image(result[0][0], orig_im_size)
+#     pil_mask_im = Image.fromarray(result_image)
+#     orig_image = Image.open(file_object)
+#     no_bg_image = orig_image.copy()
+#     no_bg_image.putalpha(pil_mask_im)
+#     return no_bg_image
+
+def io_file_input(orig_im, bytes_data):
+    orig_im_size = orig_im.shape[0:2]
+    model_input_size = [1024, 1024]
+    image = preprocess_image(orig_im, model_input_size).to(device)
+    result = model(image)
+    result_image = postprocess_image(result[0][0], orig_im_size)
+    pil_mask_im = Image.fromarray(result_image)
+    orig_image = Image.open(io.BytesIO(bytes_data))
+    no_bg_image = orig_image.copy()
+    no_bg_image.putalpha(pil_mask_im)
+    return no_bg_image
+
 
 def ass(img):
     if not hasattr(img, 'ndim'):
