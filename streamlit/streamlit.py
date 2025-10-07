a30b1e5defa74a1abbf779145dbff3ef02a23f8b
Rainbow-Ray <vkkravets03@gmail.com>
2025-10-07 23:02:26 +0500

    Streamlit

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -1,15 +1,19 @@
 from io import StringIO
-import main
+from pathlib import Path
 import streamlit as st
+import importlib.util
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+import main
+
 
 st.title('Удаление фона на фото')
 uploaded_file = st.file_uploader(label='Загрузите фотографию:', type=["jpg", "jpeg", "png"])
 if st.button('Вырезать фон'):
     if uploaded_file is not None:
-        # To read file as bytes:
         bytes_data = uploaded_file.getvalue()
 
-        # To convert to a string based IO:
+
         stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
         res = main.io_file_input(stringio)
         st.image(res)
